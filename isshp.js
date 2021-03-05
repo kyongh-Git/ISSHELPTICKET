1a4b3e3cffc292bfd878f808791bc1f75b6201a8
kyongh-Git <55642439+kyongh-Git@users.noreply.github.com>
2021-03-05 16:57:36 -0600

    Add files via upload

---
+++ isshp.js
@@ -0,0 +1,13 @@
+$(function(){
+
+var validval=$("#element_8_4").val();
+//hide the continue button when page loads
+/*if(validval=="")
+{
+$("#submit_primary").hide();
+$("#validateCWID").show();
+}*/
+
+alert(validval);
+
+});
