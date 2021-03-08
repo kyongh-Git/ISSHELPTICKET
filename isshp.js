8a3c4208277cbadf93bb3a271e545665646c8925
kyongh-Git <55642439+kyongh-Git@users.noreply.github.com>
2021-03-08 11:04:09 -0600

    Add files via upload

--- isshp.js
+++ isshp.js
@@ -3,18 +3,29 @@ $(function(){
 $('input[type="checkbox"]').click(function(){
             if($("#element_8_4").is(":checked")){
                 $("#li_7").show();
+				$("#li_9").show();
             }
             else if($("#element_8_4").is(":not(:checked)")){
                 $("#li_7").hide();
+				$("#li_9").hide();
+            }
+			if($("#element_8_15").is(":checked")){
+                $("#li_10").show();
+				$("#li_11").show();
+            }
+            else if($("#element_8_15").is(":not(:checked)")){
+                $("#li_10").hide();
+				$("#li_11").hide();
+            }
+			if($("#element_8_16").is(":checked")){
+                $("#li_12").show();
+				$("#li_12").show();
+            }
+            else if($("#element_8_16").is(":not(:checked)")){
+                $("#li_12").hide();
+				$("#li_12").hide();
             }
         });
-//hide the continue button when page loads
-/*if(validval=="")
-{
-$("#li_5").hide();
-$("#validateCWID").show();
-}*/
-
 //alert(validval);
 
 });
